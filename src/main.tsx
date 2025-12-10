e2f074eb777dd31ce670f6b074f15ef77d22fcc9
zero-3058 <myproject3058@gmail.com>
2025-12-10 21:37:54 +0530

    up

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+
 import { initTelegram } from "./telegram";
 
-initTelegram(); // Important: initializes theme, viewport, events
+initTelegram();
 
 const rootElement = document.getElementById("root");
 
